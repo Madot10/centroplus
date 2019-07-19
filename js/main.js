f51a48775b02b4069e4401ec3d14e4b3fa50e875
Madot <migueldeolim1@gmail.com>
2019-07-19 18:32:30 -0400

    Card Profesores

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 const dbId = '1eKkBkgUsMM62K6Pyl04z4YOElJQHn5OJ8AevhXR-N_Y';
+const imgDefault = 'https://image.flaticon.com/icons/png/512/23/23140.png';
+
+window.onload = ()=>{
+    loadProfesores();
+}
+
 
 //Devuelve JSON de la hoja pedida
 function getDataSheetJSON(name){
@@ -37,9 +43,79 @@ function getDataSheetJSON(name){
         request.send();
     })
 }
-/*
-getDataSheetJSON('profesores').then((response) =>{
-    console.log(response.feed.entry);
-}, (error) =>{
-    console.log(error);
-})*/
+
+// PROFESORES 
+function loadProfesores(){
+    //Obtenemos la data json
+    getDataSheetJSON('profesores').then((response) =>{
+        //console.log(response.feed.entry);
+        genCardProf(response.feed.entry);
+
+    }, (error) =>{
+        console.log(error);
+    })
+   
+}
+
+function genCardProf(jdata){
+  
+    jdata.forEach(e => {
+        console.log(e);
+        let dCard = document.createElement('div');
+        dCard.setAttribute('class', 'card');
+
+        let foto = document.createElement('img');
+        //En caso de no tener foto
+        if(e['gsx$linkfoto']['$t'] != ""){
+            foto.setAttribute('src', e['gsx$linkfoto']['$t']);
+        }else{
+            foto.setAttribute('src', imgDefault);
+        }
+        foto.setAttribute('class', 'card-img-top');
+        foto.setAttribute('alt', 'Foto de '+e['gsx$nombre']['$t']);
+
+        dCard.appendChild(foto);
+
+        let dBody = document.createElement('div');
+        dBody.setAttribute('class', 'card-body');
+
+        let cTitle = document.createElement('h5');
+        cTitle.setAttribute('class', 'card-title');
+        cTitle.innerText = e['gsx$nombre']['$t'];
+        dBody.appendChild(cTitle);
+
+        let cSub = document.createElement('h6');
+        cSub.setAttribute('class', 'card-subtitle mb-2 text-muted');
+        cSub.innerText = e['gsx$materia']['$t'];
+        dBody.appendChild(cSub);
+
+        if(e['gsx$quehace']['$t'] != ""){
+            cDo = document.createElement('p');
+            cDo.setAttribute('class', 'card-text');
+            cDo.innerText = e['gsx$quehace']['$t'];
+            dBody.appendChild(cDo);
+        }
+
+        let cText = document.createElement('p');
+        cText.setAttribute('class', 'card-text');
+        cText.innerText = "Estudios:";
+        dBody.appendChild(cText);
+
+        dCard.append(dBody);
+
+        let ul = document.createElement('ul');
+        ul.setAttribute('class', 'list-group list-group-flush');
+
+        e['gsx$estudios']['$t'].split(',').forEach(est =>{
+            let li = document.createElement('li');
+            li.setAttribute('class', 'list-group-item');
+            li.innerText = est;
+            ul.append(li);
+        });
+        dCard.append(ul);
+        
+        //Agregamos al DOM
+        document.getElementById('cardProf').appendChild(dCard);
+    });
+}
+
