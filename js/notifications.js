ee77ad271becc257e1ffacd218d7935ade41b424
Madot <migueldeolim1@gmail.com>
2019-08-10 13:47:57 -0400

    Admin Noti Basic funcional

---
+++ js/notifications.js
@@ -0,0 +1,65 @@
+const logoUrl = "/media/logoOrin.png";
+
+function textCount(){
+    document.getElementById("countChar").innerText = ` ${160 - document.getElementsByTagName("textarea")[0].textLength}/160`;
+}
+
+function getTargetTopics(){
+    let options = document.getElementById("targetTopics").selectedOptions;
+    let values = "";
+
+    for(let i = 0; i < options.length; i++){
+        if(values != ""){
+            values += `, ${options[i].value}`;
+        }else{
+            //1er vez
+            values = options[i].value;
+        }
+    }
+    //console.log(values);
+    return values;
+}
+
+function genObjNoti(){
+    //Url de acction
+    let urlAct = "/";
+    if(document.getElementById('urlActInput').value != ""){
+        urlAct = document.getElementById('urlActInput').value;
+    }
+
+    //Url imagen
+    let urlImg = "";
+    if(document.getElementById('ulrImgInput').value != ""){
+        urlImg = document.getElementById('ulrImgInput').value;
+    }
+
+    return nOptions = {
+        webpush:{
+            notification:{
+                title: document.getElementById('titleInput').value,
+                body: document.getElementById('cuerpoInput').value,
+                click_action: urlAct,
+                image: urlImg,
+                icon: logoUrl,
+                badge: logoUrl
+            }
+        },
+        token: getTargetTopics()
+    }
+}
+
+function testNotificacion(){
+    FB_CM.requestPermission().then(() =>{
+        if ('serviceWorker' in navigator) {
+            //console.log("Installing");
+            let noti = genObjNoti().webpush;
+            navigator.serviceWorker.register('/firebase-messaging-sw.js', { scope: '/firebase-cloud-messaging-push-scope' })
+                .then(function (swReg) {
+                    swReg.showNotification(noti.notification.title, noti.notification);
+                    console.log(noti.title, noti);
+                    FB_CM.useServiceWorker(swReg);
+                });
+        }
+    })
+    
+}
