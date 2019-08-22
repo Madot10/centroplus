9eb6b73526896809bebc8e8188e37a5c839b8dd5
Madot <migueldeolim1@gmail.com>
2019-08-22 17:44:40 -0400

    Manifest Basic, Sw cache

--- js/main.js
+++ js/main.js
@@ -131,6 +131,25 @@ function setWarnEmpty(state) {
     }
 }
 
+//#region SW
+// Check compatibility for the browser we're running this in
+if ("serviceWorker" in navigator) {
+    if (navigator.serviceWorker.controller) {
+      console.log("[PWA Builder] active service worker found, no need to register");
+    } else {
+      // Register the service worker
+      navigator.serviceWorker
+        .register("sw-centro.js", {
+          scope: "./"
+        })
+        .then(function (reg) {
+          console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
+        });
+    }
+  }
+  
+//#endregion
+
 //#region NAV
 function descheckNavOption() {
     let navs = document.getElementsByClassName("main-nav");
