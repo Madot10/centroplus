d91f87fb9cf65ae2f7ecade27938f9509ea8aed9
Madot <migueldeolim1@gmail.com>
2019-08-24 12:54:58 -0400

    Ga implement

--- sw-centro.js
+++ sw-centro.js
@@ -8,10 +8,10 @@ const precacheFiles = [
   '/modalnt.html?v=5',
   '/warnempty.html?v=5',
   /* PAGES */
-  '/index.html?v=6',
-  '/inicio/index.html?v=6',
-  '/admin/index.html?v=6',
-  '/admin/notificaciones/index.html?v=6',
+  '/index.html?v=8',
+  '/inicio/index.html?v=8',
+  '/admin/index.html?v=8',
+  '/admin/notificaciones/index.html?v=8',
   /* TEMPLATES */
   '/templates/archivos.html?v=1',
   '/templates/configuracion.html?v=1',
