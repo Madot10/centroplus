437245eb7fe40c4425ad9afc61387de0d63eb8ff
Madot <migueldeolim1@gmail.com>
2019-08-31 11:26:38 -0400

    Change Archivos to Recursos

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ function loadview() {
                         loadProfesores();
                         break;
 
-                    case 'archivos':
+                    case 'recursos':
                         loadArchivos();
                         break;
 
@@ -208,7 +208,7 @@ function getDataSheetJSON(name) {
         pg = 1;
     } else if (name == "salones") {
         pg = 2;
-    } else if (name == "archivos") {
+    } else if (name == "recursos") {
         pg = 3;
     }
 
@@ -505,7 +505,7 @@ function genSalones() {
 //#region ARCHIVOS
 function loadArchivos() {
     //Revisamos DataSave y tiempo pasado
-    getSaveData('archivos').then(data => {
+    getSaveData('recursos').then(data => {
         if (data.data && ((data.time.getTime() + timeLimit > new Date().getTime()) || !navigator.onLine)) {
             //Data guarda y tiempo No pasado o Data guarda y offline
             console.log("Usando data salvada");
@@ -514,15 +514,15 @@ function loadArchivos() {
 
         } else if (navigator.onLine) {
             //Obtenemos la data json
-            getDataSheetJSON('archivos').then((response) => {
+            getDataSheetJSON('recursos').then((response) => {
                 if (data) {
                     //Tiempo paso y online => Actualizamos
                     console.log("Updating Data profe");
-                    manageCaseData('update', 'archivos', new Date(), response.feed.entry);
+                    manageCaseData('update', 'recursos', new Date(), response.feed.entry);
                 } else {
                     //No hay data guardada => Traemos y salvamos para la prox
                     console.log("Guardando Data profe");
-                    manageCaseData('save', 'archivos', new Date(), response.feed.entry);
+                    manageCaseData('save', 'recursos', new Date(), response.feed.entry);
                 }
 
                 genCardFile(response.feed.entry);
